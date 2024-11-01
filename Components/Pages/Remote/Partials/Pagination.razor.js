0556e0ad5d397bea81a705220f5cd91d1569ec75
heasheartfire <gagi1995@gmail.com>
2024-11-01 19:56:02 +0100

    feat: Add pagination component

---
+++ Components/Pages/Remote/Partials/Pagination.razor.js
@@ -0,0 +1,17 @@
+﻿let Reference = {};
+Reference.DotNet = null;
+
+const keyDownHandler = (e) => {
+    if (e.repeat) return;
+    Reference.DotNet.invokeMethodAsync('OnKeyDown', e.key);
+}
+
+export function SetDotNetReference(dotnet) {
+    Reference.DotNet = dotnet;
+
+    window.addEventListener('keydown', keyDownHandler);
+}
+
+export function Dispose() {
+    window.removeEventListener('keydown', keyDownHandler);
+}
