28c28631a354c8dab90b5acd01a3545bfd4cfcd7
heasheartfire <gagi1995@gmail.com>
2024-11-18 18:24:18 +0100

    refactor(AppStateService)

--- Services/AppStateService.cs
+++ Services/AppStateService.cs
@@ -4,29 +4,35 @@ public class AppStateService
 {
     public string? ServerUrl
     {
-        get => Get(nameof(ServerUrl)) as string;
+        get => Get<string>(nameof(ServerUrl));
         set => Set(nameof(ServerUrl), value);
     }
     
     public string? Language
     {
-        get => Get(nameof(Language)) as string;
+        get => Get<string>(nameof(Language));
         set => Set(nameof(Language), value);
     }
     
     public Guid ProfileId
     {
-        get => Guid.TryParse(Get(nameof(ProfileId))?.ToString(), out var result) ? result : Guid.Empty;
-        set => Set(nameof(ProfileId), value.ToString());
+        get => Get<Guid>(nameof(ProfileId));
+        set => Set(nameof(ProfileId), value);
     }
     
-    private void Set(string key, dynamic? value)
+    private static void Set<T>(string key, T value)
     {
-        Preferences.Set(key, value);
+        Preferences.Set(key, value!.ToString());
     }
 
-    private object? Get(string key)
+    private static T? Get<T>(string key)
     {
-        return Preferences.Get(key, null);
+        object? value = Preferences.Get(key, null);
+        var type = typeof(T);
+        return type switch
+        {
+            _ when type == typeof(Guid) => (T)(object)Guid.Parse(value?.ToString() ?? Guid.Empty.ToString()),
+            _ => (T?)value
+        };
     }
 }
