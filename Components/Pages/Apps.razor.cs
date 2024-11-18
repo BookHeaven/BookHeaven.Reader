016b6a83e44971f877b72890589104db7cd1c315
heasheartfire <gagi1995@gmail.com>
2024-11-18 18:22:20 +0100

    refactor(Apps.razor)

--- Components/Pages/Apps.razor.cs
+++ Components/Pages/Apps.razor.cs
@@ -6,32 +6,31 @@ public partial class Apps
 {
     private List<AppInfo> _apps = new();
     private List<AppInfo> _filteredApps = new();
-
-    private Sorting _sortBy = Sorting.Added;
-
-    protected override void OnInitialized()
-    {
-        _apps = AppsService.GetInstalledApps();
-        Sort(_sortBy);
-    }
-
-    private void Sort(Sorting sort)
-    {
-        _sortBy = sort;
-        switch (_sortBy)
-        {
-            case Sorting.Added:
-                _filteredApps = _apps.OrderBy(x => x.FirstInstallTime).ToList();
-                break;
-            case Sorting.Name:
-                _filteredApps = _apps.OrderBy(x => x.Name).ToList();
-                break;
-        }
-    }
-
-    private enum Sorting
+    
+    private enum SortBy
     {
         Added,
         Name
     }
+
+    private SortBy _sortBy = SortBy.Added;
+
+    protected override void OnInitialized()
+    {
+        _apps = AppsService.GetInstalledApps();
+        _sortBy = SortBy.Added;
+        Sort();
+    }
+
+    private void Sort()
+    {
+        _filteredApps = _sortBy switch
+        {
+            SortBy.Added => _apps.OrderBy(x => x.FirstInstallTime).ToList(),
+            SortBy.Name => _apps.OrderBy(x => x.Name).ToList(),
+            _ => _filteredApps
+        };
+    }
+
+    
 }
