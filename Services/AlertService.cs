84f36789728cbe5aa7d4701b9203821b9a8de077
heasheartfire <gagi1995@gmail.com>
2024-12-11 23:19:11 +0100

    feat(AlertService): Add service to use native alerts and dialogs on blazor

---
+++ Services/AlertService.cs
@@ -0,0 +1,34 @@
+﻿namespace BookHeaven.Reader.Services;
+
+public class AlertService
+{
+    public Task ShowAlertAsync(string title, string message, string cancel = "OK")
+    {
+        return Application.Current!.Windows[0].Page!.DisplayAlert(title, message, cancel);
+    }
+
+    public Task<bool> ShowConfirmationAsync(string title, string message, string accept = "Yes", string cancel = "No")
+    {
+        return Application.Current!.Windows[0].Page!.DisplayAlert(title, message, accept, cancel);
+    }
+
+    public Task<string> ShowPromptAsync(string title, string message, string accept = "Yes", string cancel = "No")
+    {
+        return Application.Current!.Windows[0].Page!.DisplayPromptAsync(title, message, accept, cancel,maxLength:100);
+    }
+    
+    public void ShowAlert(string title, string message, string cancel = "OK")
+    {
+        Application.Current!.Windows[0].Page!.Dispatcher.DispatchAsync(async () =>
+            await ShowAlertAsync(title, message, cancel)
+        );
+    }
+    public void ShowConfirmation(string title, string message, Action<bool> callback, string accept = "Yes", string cancel = "No")
+    {
+        Application.Current!.Windows[0].Page!.Dispatcher.DispatchAsync(async () =>
+        {
+            var answer = await ShowConfirmationAsync(title, message, accept, cancel);
+            callback(answer);
+        });
+    }
+}
