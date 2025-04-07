0815bd64ae1c8f16983bc938eeb4af1a6a566333
heasheartfire <gagi1995@gmail.com>
2025-04-07 23:44:43 +0200

    feat: Add broadcasting capabilities to avoi writing the server's url on the client

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -5,53 +5,52 @@ using BookHeaven.Reader.Services;
 using CommunityToolkit.Maui;
 using EpubManager;
 using Microsoft.Extensions.Logging;
-using DependencyInjection = BookHeaven.Domain.DependencyInjection;
 
-namespace BookHeaven.Reader
+namespace BookHeaven.Reader;
+
+public static class MauiProgram
 {
-	public static class MauiProgram
-	{
-		public static readonly string BooksPath = Path.Combine(FileSystem.AppDataDirectory, "books");
-		public static readonly string CoversPath = Path.Combine(FileSystem.AppDataDirectory, "covers");
-		public static readonly string CachePath = Path.Combine(FileSystem.AppDataDirectory, "cache");
-		public static readonly string FontsPath = Path.Combine(FileSystem.AppDataDirectory, "fonts");
+	public static readonly string BooksPath = Path.Combine(FileSystem.AppDataDirectory, "books");
+	public static readonly string CoversPath = Path.Combine(FileSystem.AppDataDirectory, "covers");
+	public static readonly string CachePath = Path.Combine(FileSystem.AppDataDirectory, "cache");
+	public static readonly string FontsPath = Path.Combine(FileSystem.AppDataDirectory, "fonts");
 		
-		public static MauiApp CreateMauiApp()
-		{
-            Directory.CreateDirectory(BooksPath);
-			Directory.CreateDirectory(CoversPath);
-			Directory.CreateDirectory(CachePath);
-			Directory.CreateDirectory(FontsPath);
+	public static MauiApp CreateMauiApp()
+	{
+		Directory.CreateDirectory(BooksPath);
+		Directory.CreateDirectory(CoversPath);
+		Directory.CreateDirectory(CachePath);
+		Directory.CreateDirectory(FontsPath);
 
-			var builder = MauiApp.CreateBuilder();
-			builder
-				.UseMauiApp<App>()
-				.UseMauiCommunityToolkit();
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit();
 
-			builder.Services.AddDomain(FileSystem.AppDataDirectory);
-			builder.Services.AddEpubManager(true);
+		builder.Services.AddDomain(FileSystem.AppDataDirectory);
+		builder.Services.AddEpubManager(true);
 			
 			
-			builder.Services.AddSingleton<AppStateService>();
-			builder.Services.AddSingleton<LifeCycleService>();
+		builder.Services.AddSingleton<AppStateService>();
+		builder.Services.AddSingleton<LifeCycleService>();
+		builder.Services.AddSingleton<UdpBroadcastClient>();
 
-			builder.Services.AddScoped<BookManager>();
-			builder.Services.AddScoped<IAppsService, AppsService>();
-			builder.Services.AddScoped<ImageViewerService>();
-			builder.Services.AddScoped<OverlayService>();
+		builder.Services.AddScoped<BookManager>();
+		builder.Services.AddScoped<IAppsService, AppsService>();
+		builder.Services.AddScoped<ImageViewerService>();
+		builder.Services.AddScoped<OverlayService>();
 			
-			builder.Services.AddTransient<IServerService, ServerService>();
-			builder.Services.AddTransient<AlertService>();
+		builder.Services.AddTransient<IServerService, ServerService>();
+		builder.Services.AddTransient<AlertService>();
 
-			builder.Services.AddBlazorContextMenu();
-			builder.Services.AddMauiBlazorWebView();
-			builder.Services.AddBlazorPanzoomServices();
+		builder.Services.AddBlazorContextMenu();
+		builder.Services.AddMauiBlazorWebView();
+		builder.Services.AddBlazorPanzoomServices();
 
 #if DEBUG
-			builder.Services.AddBlazorWebViewDeveloperTools();
-			builder.Logging.AddDebug();
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
 #endif
-			return builder.Build();
-		}
+		return builder.Build();
 	}
 }
---
+++ Services/UdpBroadcastClient.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using BookHeaven.Domain.Constants;
+
+namespace BookHeaven.Reader.Services;
+
+public class UdpBroadcastClient(AppStateService appStateService)
+{
+    public async Task StartAsync()
+    {
+        using var udpClient = new UdpClient
+        {
+            EnableBroadcast = true
+        };
+
+        var broadcastAddress = new IPEndPoint(IPAddress.Broadcast, Broadcast.BROADCAST_PORT);
+        var discoverMessage = Encoding.UTF8.GetBytes(Broadcast.DISCOVER_MESSAGE);
+
+        await udpClient.SendAsync(discoverMessage, discoverMessage.Length, broadcastAddress);
+
+        using var listener = new UdpClient(Broadcast.BROADCAST_PORT);
+
+        while (true)
+        {
+            var result = await listener.ReceiveAsync();
+            var responseMessage = Encoding.UTF8.GetString(result.Buffer);
+
+            if (!responseMessage.StartsWith(Broadcast.SERVER_URL_MESSAGE_PREFIX))
+            {
+                await Task.Delay(2000);
+                continue;
+            }
+            
+            var parts = responseMessage.Split(':');
+            
+            var serverUrl = parts[1];
+
+            // Enviar ACK al servidor
+            var ackMessage = Encoding.UTF8.GetBytes(Broadcast.ACK_MESSAGE);
+            await udpClient.SendAsync(ackMessage, ackMessage.Length, result.RemoteEndPoint);
+            return;
+        }
+        
+    }
+}
