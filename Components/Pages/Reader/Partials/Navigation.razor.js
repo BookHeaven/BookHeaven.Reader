109c6cb621875aaf17d9503ebea6a80922ff823c
heasheartfire <gagi1995@gmail.com>
2025-04-12 02:52:09 +0200

    fix(ImageViewer): Fixed crash and modified to work with both img and image

--- Components/Pages/Reader/Partials/Navigation.razor.js
+++ Components/Pages/Reader/Partials/Navigation.razor.js
@@ -1,8 +1,8 @@
 ﻿export function FindZoomableElement(x, y) {
     let elements = document.elementsFromPoint(x, y);
-    let element = elements.find(e => e.className.includes("zoomable"));
+    let element = elements.find(e => e.classList.value.includes("zoomable"));
     if (element) {
-        return element.attributes["src"].value;
+        return element.attributes["src"] ? element.attributes["src"].value : element.attributes["href"].value;
     }
     return null;
 }
