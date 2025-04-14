01090a8a2640f0542eef39d962e012413134625c
heasheartfire <gagi1995@gmail.com>
2025-04-14 17:36:28 +0200

    refactor(Books): Move actions from book info to context menu, now book can be marked as finished

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -9,11 +9,13 @@ using MediatR;
 
 namespace BookHeaven.Reader.Services;
 
-public class BookManager(ISender sender)
+public class BookManager(
+    AlertService alertService,
+    ISender sender)
 {
     public List<Book> Books { get; set; } = [];
 
-    public async Task ClearCache(Book book, bool showToast = true)
+    private async Task ClearCache(Book book, bool showToast = true)
     {
         var progress = book.GetCachePath(CacheKey.Progress);
         var styles = book.GetCachePath(CacheKey.Styles);
@@ -30,23 +32,11 @@ public class BookManager(ISender sender)
         if(showToast) await Toast.Make("Cache cleared").Show();
     }
     
-    public async Task ResetProgress(Book book)
-    {
-        var progress = new BookProgress()
-        {
-            BookProgressId = book.Progress().BookProgressId,
-            ProfileId = book.Progress().ProfileId,
-            BookId = book.BookId,
-        };
-        
-        await sender.Send(new UpdateBookProgress.Command(progress));
-
-        await ClearCache(book, false);
-        await Toast.Make("Progress has been reset").Show();
-    }
-    
     public async Task DeleteBook(Book book)
     {
+        var result = await alertService.ShowConfirmationAsync("Delete book", $"Are you sure you want to delete this book?{Environment.NewLine}{Environment.NewLine}This will remove the book from your device along with any progress you have.{Environment.NewLine}It will not be removed from your server.");
+        if (!result) return;
+        
         var epubPath = book.EpubPath(MauiProgram.BooksPath);
         var coverPath = book.CoverPath(MauiProgram.CoversPath);
         
@@ -62,5 +52,40 @@ public class BookManager(ISender sender)
         await sender.Send(new DeleteBook.Command(book.BookId));
         Books.Remove(book);
         await Toast.Make("Book has been deleted").Show();
+    }
+    
+    public async Task MarkAsNew(Book book)
+    {
+        var result = await alertService.ShowConfirmationAsync("Are you sure?", "This will reset your progress, which can't be undone unless you delete the book and download it again.");
+        if (!result) return;
+        
+        var progress = book.Progress();
+        progress.StartDate = null;
+        progress.EndDate = null;
+        progress.Progress = 0;
+        progress.ElapsedTime = TimeSpan.Zero;
+        progress.LastRead = null;
+        progress.Chapter = 0;
+        progress.Page = 0;
+        progress.PageCount = 0;
+        progress.PageCountNext = 0;
+        progress.PageCountPrev = 0;
+        await sender.Send(new UpdateBookProgress.Command(progress));
+        await ClearCache(book, false);
+        await Toast.Make("Book has been marked as new").Show();
+    }
+    
+    public async Task MarkAsFinished(Book book)
+    {
+        var progress = book.Progress();
+        if(progress.ElapsedTime == TimeSpan.Zero)
+        {
+            progress.StartDate = DateTimeOffset.Now;
+        }
+        progress.EndDate = DateTimeOffset.Now;
+        progress.Progress = 100;
+        await sender.Send(new UpdateBookProgress.Command(progress));
+        await ClearCache(book, false);
+        await Toast.Make("Book has been marked as finished").Show();
     }
 }
