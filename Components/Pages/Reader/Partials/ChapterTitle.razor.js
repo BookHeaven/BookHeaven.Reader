42dcc7bb7850693c06dd06405a10ccec970dd56f
heasheartfire <gagi1995@gmail.com>
2025-08-07 18:50:16 +0200

    feat(ChapterTitle): Improve center ellipsis overflow logic by dynamically measuring total and available space

---
+++ Components/Pages/Reader/Partials/ChapterTitle.razor.js
@@ -0,0 +1,13 @@
+﻿export async function measureTextWidth(element, text) {
+    const style = getComputedStyle(element);
+    const font = `${style.fontWeight} ${style.fontSize} ${style.fontFamily}`;
+
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.font = font;
+    return ctx.measureText(text).width;
+}
+
+export function getElementWidth(element) {
+    return element.offsetWidth;
+}
