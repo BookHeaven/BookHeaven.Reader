f9c608c7ca87938838aafccdbd9e80ba403b1491
heasheartfire <gagi1995@gmail.com>
2026-02-07 19:28:45 +0100

    feat(MainActivity): don't force portrait orientation

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.Content.PM;
 
 namespace BookHeaven.Reader;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density, ScreenOrientation = ScreenOrientation.Portrait, Exported = true)]
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density, ScreenOrientation = ScreenOrientation.Unspecified, Exported = true)]
 [IntentFilter([Intent.ActionMain], Categories = [Intent.CategoryDefault, Intent.CategoryHome, Intent.CategoryLauncher])]
 public class MainActivity : MauiAppCompatActivity
 {
